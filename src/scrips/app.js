eabfad7c286a669f3ab750110e5404191ec271aa
DvAndresGT <diegogiraldo506@gmail.com>
2025-07-27 22:13:15 -0500

    0.0.1
    implementar función de sorteo de amigo secreto con validaciones y visualización en DOM

--- src/scrips/app.js
+++ src/scrips/app.js
@@ -1,1 +1,39 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+  let inputNombre = document.getElementById("amigo");
+  if (inputNombre.value.trim() !== "") {
+    amigos.push(inputNombre.value);
+    inputNombre.value = "";
+    mostrarAmigos();
+  } else {
+    alert("Por favor, ingrese un nombre");
+  }
+}
+
+function mostrarAmigos() {
+  let amigosList = document.getElementById("listaAmigos");
+  amigosList.innerHTML = "";
+  for (let i = 0; i < amigos.length; i++) {
+    let li = document.createElement("li");
+    li.textContent = amigos[i];
+    amigosList.appendChild(li);
+  }
+}
+
+function sortearAmigo() {
+  if (amigos.length < 2) {
+    alert("Debe haber al menos dos amigos para sortear.");
+    return;
+  }
+  if (amigos.length > 10) {
+    alert("No se pueden sortear más de 10 amigos.");
+    return;
+  }
+  const indice = Math.floor(Math.random() * amigos.length);
+  const amigoSorteado = amigos[indice];
+  const resultadoLista = document.getElementById("resultado");
+  const li = document.createElement("li");
+  li.textContent = "Tu amigo secreto es " + amigoSorteado;
+  resultadoLista.appendChild(li);
+}
